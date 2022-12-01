9c5ab35a7870941ee920e4b5ac284db45b233702
Sayko Andrey <Saykotechka76@mail.ru>
2022-12-01 11:58:42 +0300

    10lab

--- 9laba/AutoForTestCase/AutoForTestCase/UnitTest1.cs
+++ 9laba/AutoForTestCase/AutoForTestCase/UnitTest1.cs
@@ -11,12 +11,14 @@ namespace FunctionalTests
         public void ViewScoreboardOfMinskStation()
         {
             IWebDriver driver = new ChromeDriver();
-            driver.Url = "https://www.aviasales.by/search/MSQ1511MOW1?request_source=search_form";
+            driver.Url = "https://www.aviasales.by/search/MSQ0612MOW1?request_source=best_prices";
             driver.Manage().Window.Maximize();
-            driver.FindElement(By.XPath("/html/body/div[5]/div/div/div[3]/div/div/div/div/div[2]/div[2]/div[2]/div[2]/div/div/div[1]/div/div[2]/div/div")).Click();
+            driver.FindElement(By.XPath("/html/body/div[5]/div/div/div[3]/div/div/div/div/div/div[2]/div[2]/div[2]/div/div/div[1]/div/div[2]/div/div/button")).Click();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-            driver.FindElement(By.XPath("/html/body/div[22]/div/div/div/div/div[2]/div/div/div[1]/div[4]/div/a")).Click();
+            driver.FindElement(By.XPath("/html/body/div[23]/div/div/div/div/div[2]/div/div/div[1]/div[4]/div/a")).Click();
             driver.Close();
         }
+
+
     }
 }
